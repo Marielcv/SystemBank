d200dd32852c62ea037647722a91fdb9031218e3
Mariel AH <113214747+Marielcv@users.noreply.github.com>
2022-10-23 13:14:03 -0600

    app
    commit

--- app/src/main/res/layout/Names.kt
+++ app/src/main/res/layout/Names.kt
@@ -2,7 +2,7 @@ package layout
 
 import org.w3c.dom.NameList
 
-class Names(val names:String,var edad:Int, val Name:Array<NamesProgram>,val amigo:Array<Names>?=null) {
+class Names(val names:String,var edad:Int, val Name:Array<NamesProgram>,val name:Array<Names>?=null) {
     enum class NamesProgram{
         MARIA,
         ALEXANDRA,
